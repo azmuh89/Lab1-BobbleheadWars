2b03ff80db09c152f0aee02c9100e3ae03764da0
Humza Khan <humza.khan@msn.com>
2019-09-25 17:28:20 -0400

    Added in PlayerMovement & CameraFollow scripts

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject followTarget;
+    public float moveSpeed;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if (followTarget != null)
+        {
+            transform.position = Vector3.Lerp(transform.position, followTarget.transform.position, Time.deltaTime * moveSpeed);
+        }
+    }
+}
