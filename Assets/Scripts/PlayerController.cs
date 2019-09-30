3b510acdb897736d041f895e3c7de40bbce9dfd1
Humza Khan <humza.khan@msn.com>
2019-09-30 11:39:50 -0400

    changed player movement and added layers

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,18 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 50.0f;
+    private CharacterController characterController;
 
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 pos = transform.position;
-        pos.x += moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
-        pos.z += moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
-        transform.position = pos;
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); // creates new vector3 to store movement direction
+        characterController.SimpleMove(moveDirection * moveSpeed); // built-in method that automatically moves character in given direction but not allowing to move through obstacles
     }
 }
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject); // Destroy bullets when they go off-screen
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject); // Destroy bullets when the collide with other objects
+    }
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
